82d072b8ea61eff07dab2d3fa8e3192576f6d69d
Siddharth Bhandari <sidzzatwork22@gmail.com>
2025-06-30 00:49:15 +0530

    Source Code upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Todo from "./Todo";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1>📝 React To-Do List</h1>
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
